ec18c6cfb6e3ff41c3b729786173a4bfd492bdd3
lee seonguk <passion1014@gmail.com>
2024-07-30 13:45:49 +0900

    Revert "Reapply "commit""
    This reverts commit 46c0f3047fbe67f32a64073a3e8fa44c79e2a6e0.

--- app/server.py
+++ app/server.py
@@ -2,12 +2,7 @@ from fastapi import FastAPI
 from langserve import add_routes
 from app.config import setup_logging
 from app.chain import create_chain
-from langgraph.graph import END, StateGraph
-from IPython.display import Image, display
-from langfuse.callback import CallbackHandler
-
-# Langfuse CallbackHandler 초기화
-langfuse_handler = CallbackHandler()
+# from langgraph.graph import END, StateGraph, MessagesState
 
 # 로깅 설정
 logger = setup_logging()
@@ -16,22 +11,11 @@ logger = setup_logging()
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
-    description="Spin up a simple api server using Langchain's Runnable interfaces with LangGraph",
+    description="Spin up a simple api server using Langchain's Runnable interfaces",
 )
 
 # 체인 생성
-chain = create_chain().with_config(callbacks=[langfuse_handler])
-
-    
-try:
-    # 실행 가능한 객체의 그래프를 mermaid 형식의 PNG로 그려서 표시합니다. 
-    display(
-        # xray=True는 추가적인 세부 정보를 포함합니다.
-        Image(chain.get_graph(xray=True).draw_mermaid_png())
-    ) 
-except:
-    pass
-
+chain = create_chain()
 
 # 라우트 추가
 add_routes(app, chain, path="/prompt", enable_feedback_endpoint=True)
