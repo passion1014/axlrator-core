47a55d61b258f887a707f4ac44b515aed91e80ca
lee seonguk <passion1014@gmail.com>
2024-10-15 09:57:09 +0900

    ㅊ

--- test/test_route.py
+++ test/test_route.py
@@ -1,4 +1,7 @@
 import requests
 
-response = requests.post("http://localhost:8000/plugin/api/sql", json={"question": "SELECT * FROM users;"})
+response = requests.post("http://localhost:8000/plugin/api/sql", json={"question": "보증 조회 쿼리"})
+print(response.json())
+
+response = requests.post("http://localhost:8000/plugin/api/code", json={"question": "윤달 프로그램을 작성해줘"})
 print(response.json())
