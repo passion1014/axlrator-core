ccdc6b77d0a978dadcc227845dd19d68728bb562
lee seonguk <passion1014@gmail.com>
2024-11-08 15:30:01 +0900

    commit

--- app/server.py
+++ app/server.py
@@ -4,11 +4,11 @@ from langserve import add_routes
 from app.chain import create_anthropic_chain, create_openai_chain, create_rag_chain, create_term_conversion_chain, create_text_to_sql_chain
 from fastapi import FastAPI
 from app.config import STATIC_DIR
-from app.routes.upload_routes import router as upload_router
-from app.routes.faiss_routes import router as faiss_router
+from app.routes.upload_routes import router as upload_routes
+from app.routes.faiss_routes import router as faiss_routes
 from app.routes.eclipse_routes import router as eclipse_router
-from app.routes.sample_routes import router as sample_router
-from app.routes.completion_routes import router as completion_router
+from app.routes.sample_routes import router as sample_routes
+from app.routes.terms_conversion_routes import router as terms_conversion_routes
 
 import uvicorn
 
@@ -26,18 +26,19 @@ webServerApp.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 # ---------------------------------------
 # 라우터 등록
 # ---------------------------------------
-webServerApp.include_router(upload_router, prefix="/upload") # 업로드 라우터 등록
-webServerApp.include_router(faiss_router, prefix="/faiss") # faiss 라우터 등록
-webServerApp.include_router(eclipse_router, prefix="/plugin") # eclipse plugin 라우터 등록
-webServerApp.include_router(completion_router, prefix="/completion") # 자동완성 화면 라우터 등록
-webServerApp.include_router(sample_router, prefix="/sample")
+webServerApp.include_router(upload_routes, prefix="/upload") # 업로드 라우터
+webServerApp.include_router(faiss_routes, prefix="/faiss") # faiss 라우터
+webServerApp.include_router(terms_conversion_routes, prefix="/termsconversion") # 용어변환을 위한 라우터
+webServerApp.include_router(sample_routes, prefix="/sample") # <-- 해당 파일과 라우트들은 삭제 예정
+
+# 아래는 삭제 - 플러그인용으로 따로 만들지 않고 도메인에 따라 관리
+# webServerApp.include_router(eclipse_router, prefix="/plugin") # eclipse plugin 라우터 등록
 
 # 체인 등록
-add_routes(webServerApp, create_text_to_sql_chain(), path="/sql", enable_feedback_endpoint=True)
-add_routes(webServerApp, create_rag_chain(), path="/rag", enable_feedback_endpoint=True)
-add_routes(webServerApp, create_term_conversion_chain(), path="/terms", enable_feedback_endpoint=True)
-add_routes(webServerApp, create_openai_chain(), path="/openai", enable_feedback_endpoint=True)
-add_routes(webServerApp, create_anthropic_chain(), path="/anthropic", enable_feedback_endpoint=True)
+# add_routes(webServerApp, create_text_to_sql_chain(), path="/sql", enable_feedback_endpoint=True)
+# add_routes(webServerApp, create_rag_chain(), path="/rag", enable_feedback_endpoint=True)
+# add_routes(webServerApp, create_openai_chain(), path="/openai", enable_feedback_endpoint=True)
+# add_routes(webServerApp, create_anthropic_chain(), path="/anthropic", enable_feedback_endpoint=True)
 
 # 플러그인 용
 
