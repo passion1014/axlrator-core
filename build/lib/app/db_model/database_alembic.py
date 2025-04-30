0a9fd84de616e8d19dfb956c4090caeea6a65183
lee seonguk <passion1014@gmail.com>
2025-04-30 14:19:06 +0900

    commit

--- build/lib/app/db_model/database_alembic.py
+++
@@ -1,5 +0,0 @@
-# alembic으로 모델 정보 업데이트 하기 위한 용도
-
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
--- build/lib/test/test_route.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.post("http://localhost:8000/plugin/api/sql", json={"question": "보증 조회 쿼리"})
-print(response.json())
-
-response = requests.post("http://localhost:8000/plugin/api/code", json={"question": "윤달 프로그램을 작성해줘"})
-print(response.json())
