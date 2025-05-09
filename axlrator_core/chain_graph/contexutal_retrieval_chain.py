b1c0d6aa5d4d954e708b5825d4d5737734d7d5e9
lee seonguk <passion1014@gmail.com>
2025-05-09 15:02:59 +0900

    core

--- axlrator_core/chain_graph/contexutal_retrieval_chain.py
+++ axlrator_core/chain_graph/contexutal_retrieval_chain.py
@@ -1,26 +1,43 @@
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph, END
+from langfuse import Langfuse
+# from langfuse.callback import CallbackHandler
+from langchain_core.prompts import PromptTemplate
 from axlrator_core.utils import get_llm_model
 
-from langfuse import Langfuse
-from langfuse.callback import CallbackHandler
-from langchain_core.prompts import PromptTemplate
+class CreateSummaryState(TypedDict):
+    prompt: str
+    content: str
+    response: str
 
 
-def create_summary_chain():
+def chain_create_summary():
     """
     chunk를 받아서 summary를 만들어줌
     """
-    langfuse = Langfuse()
-    langfuse_prompt = langfuse.get_prompt("AUTO_CODE_TASK_PROMPT", version=1)
+    # callback_handler = CallbackHandler()
+
+    def _get_summary_prompt(state: CreateSummaryState) -> CreateSummaryState:
+        langfuse_prompt = Langfuse().get_prompt("CODE_SUMMARY_GENERATE_PROMPT")
+        state['prompt'] = langfuse_prompt.compile(
+            CODE_CHUNK=state.get('content', '')
+        )
+        return state
+
+    def _generate_summary(state: CreateSummaryState) -> CreateSummaryState:
+        result = get_llm_model().invoke(state['prompt']) 
+        state['response'] = result.content
+        return state
     
-    CODE_SUMMARY_GENERATE_PROMPT = PromptTemplate.from_template(
-        langfuse_prompt.get_langchain_prompt(),
-        metadata={"langfuse_prompt": langfuse_prompt},
-    )
+    graph = StateGraph(CreateSummaryState)
+    graph.add_node("get_summary_prompt", _get_summary_prompt)
+    graph.add_node("generate_summary", _generate_summary)
     
-    prompt_chain = (
-        CODE_SUMMARY_GENERATE_PROMPT | get_llm_model().with_config(callbacks=[CallbackHandler()])
-    )
-    return prompt_chain
+    graph.set_entry_point("get_summary_prompt")
+    graph.add_edge("get_summary_prompt", "generate_summary")
+    graph.add_edge("generate_summary", END)
+
+    return graph.compile()
 
 
 # 설계
--- axlrator_core/process/contextual_process.py
+++ axlrator_core/process/contextual_process.py
@@ -1,24 +1,25 @@
 
 
 import json
-from axlrator_core.chain_graph.contexutal_retrieval_chain import create_summary_chain
+from axlrator_core.chain_graph.contexutal_retrieval_chain import chain_create_summary
 from axlrator_core.formatter.code_formatter import parse_augmented_chunk
+from langfuse.callback import CallbackHandler
 
-
-def  generate_code_context(chunk_content:str):
+def generate_code_context(chunk_content:str):
     """chunk를 받아서 LLM을 사용하여 summary를 만듬"""
+    callback_handler = CallbackHandler()
 
     # create_summary_chain 호출
-    summary_chain = create_summary_chain()
+    summary_chain = chain_create_summary()
     
     # 요약 생성을 위한 프롬프트 입력
     inputs = {
         "SOURCE_CODE": chunk_content
     }
     
-    summary_ai_message = summary_chain.invoke(inputs) # result = AIMessage 타입
-    parsed_ai_message = parse_augmented_chunk(summary_ai_message.content)
-    parsed_json_message = json.loads(parsed_ai_message.model_dump_json())  # Pydantic v2의 기본 json 메서드를 사용해 JSON으로 변환
-    result = json.dumps(parsed_json_message, ensure_ascii=False, indent=4)
+    summary_ai_message = summary_chain.invoke(inputs, config={"callbacks": [callback_handler]})
+    # parsed_ai_message = parse_augmented_chunk(summary_ai_message.content)
+    # parsed_json_message = json.loads(parsed_ai_message.model_dump_json())  # Pydantic v2의 기본 json 메서드를 사용해 JSON으로 변환
+    # result = json.dumps(parsed_json_message, ensure_ascii=False, indent=4)
 
-    return result
+    return summary_ai_message
